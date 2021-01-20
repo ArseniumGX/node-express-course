fff35943ab43ce1ce6886e34ef911fac6d50bddf
ArseniumGX <arseniumgx@gmail.com>
2021-01-20 11:52:01 -0300

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
 ]
 
+// all GET request
 app.get('/users', (req, res) => {
     res.json({
         success: true,
@@ -21,6 +26,27 @@ app.get('/users/:id', (req, res) => {
         message: 'got one user',
         user: req.params.id
     })
+})
+
+// all POST request
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    
+    const mockUsername = "billyTheKid"
+    const mockPassword = "superSecret"
+
+    if(username === mockUsername && password === mockPassword)
+        res.json({
+            success: true,
+            message: 'username and passwors match!',
+            token: 'encrypted token goes here'
+        })
+    else
+        res.json({
+            sucess: false,
+            message: `username or password don't match`
+        })
 })
 
 app.listen(8000, () => console.log(`Server is running`));
